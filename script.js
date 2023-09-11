6273381483180a108a99133651ae03d49d281eea
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-11 20:50:49 +0200

    Add
    vue js app

--- script.js
+++ script.js
@@ -2,4 +2,20 @@
 
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
 
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
+
 */
+
+
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue JS!',
+      img: 'https://picsum.photos/200/300' 
+      
+    }
+  }
+}).mount('#app')
